f49b3763f0b0b2d38a7eaf2e342b29717d617566
taseikyo <taseikyo@gmail.com>
2019-12-01 21:04:01 +0800

    add 003_convert_mp4_to_ts_and_merge.py

---
+++ Python/003_convert_mp4_to_ts_and_merge.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-09-14 17:55:55
+# @Author  : Lewis Tian (taseikyo@gmail.com)
+# @Link    : https://github.com/taseikyo
+# @Version : Python3.7
+
+import os
+
+"""
+1. convert mp4 to ts
+2. merge ts to mp4
+"""
+
+
+def convert():
+    """convert mp4 to ts
+    """
+    for x in os.listdir("."):
+        if x.endswith("mp4"):
+            cmd = f"ffmpeg -i {x} -vcodec copy -acodec copy -vbsf h264_mp4toannexb {x.split('.')[0]}.ts"
+            os.system(cmd)
+
+
+def merge():
+    """merge ts as mp4
+    """
+    files = []
+    for x in os.listdir("."):
+        if x.endswith("ts"):
+            files.append(x)
+    if not files:
+        return
+    cmd = f"""ffmpeg -i "concat:{'|'.join(files)}" -acodec copy -vcodec copy -absf aac_adtstoasc output.mp4"""
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    convert()
+    merge()
