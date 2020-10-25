b3581fc5808f34155b08bc53e67e7047a5bdc53a
Lewis Tian <taseikyo@gmail.com>
2020-10-25 16:26:58 +0800

    [py] update 006

--- Python/006_auto_generate_toc.py
+++ Python/006_auto_generate_toc.py
@@ -15,6 +15,7 @@ Usage:
 """
 
 import sys
+import os
 
 
 # @HAS_L1_TOC is True means your md file contant `# h1`
@@ -22,6 +23,22 @@ import sys
 # if @HAS_L1_TOC is False
 # then h2 indent is zero '\t'
 HAS_L1_TOC = False
+
+ILLEGAL_CHARS = [
+    ".",
+    '"',
+    "'",
+    "，",
+    "。",
+    "、",
+    "？",
+    "：",
+    "`",
+    ":",
+    "<",
+    ">",
+    "/",
+]
 
 
 def auto_generate_toc(filename):
@@ -31,36 +48,58 @@ def auto_generate_toc(filename):
 
     toc = []
     top_level = 4 if HAS_L1_TOC else 3
+    has_toc = False
 
     for line in lines:
+        jmp_anchor = line.rstrip().lower()
+        for ch in ILLEGAL_CHARS:
+            jmp_anchor = jmp_anchor.replace(ch, "")
+
         if line.startswith("#####"):
             line = line.replace("##### ", "").strip()
+            jmp_anchor = jmp_anchor.replace("##### ", "").replace(" ", "-")
             indent = "\t" * top_level
-            toc.append(f"{indent}- [{line}](#{line.replace(' ', '-').lower()})")
+            toc.append(f"{indent}- [{line}](#{jmp_anchor})")
         elif line.startswith("####"):
             line = line.replace("#### ", "").strip()
+            jmp_anchor = jmp_anchor.replace("#### ", "").replace(" ", "-")
             indent = "\t" * (top_level - 1)
-            toc.append(f"{indent}- [{line}](#{line.replace(' ', '-').lower()})")
+            toc.append(f"{indent}- [{line}](#{jmp_anchor})")
         elif line.startswith("###"):
             line = line.replace("### ", "").strip()
+            jmp_anchor = jmp_anchor.replace("### ", "").replace(" ", "-")
             indent = "\t" * (top_level - 2)
-            toc.append(f"{indent}- [{line}](#{line.replace(' ', '-').lower()})")
+            toc.append(f"{indent}- [{line}](#{jmp_anchor})")
         elif line.startswith("##"):
             line = line.replace("## ", "").strip()
+            jmp_anchor = jmp_anchor.replace("## ", "").replace(" ", "-")
             indent = "\t" * (top_level - 3)
-            toc.append(f"{indent}- [{line}](#{line.replace(' ', '-').lower()})")
+            toc.append(f"{indent}- [{line}](#{jmp_anchor})")
         elif line.startswith("#"):
+            """
+            #include
+            #define
+            """
+            if line[1] != " ":
+                continue
             line = line.replace("# ", "").strip()
+            jmp_anchor = jmp_anchor.replace("# ", "").replace(" ", "-")
             indent = "\t" * (top_level - 4)
-            toc.append(f"{indent}- [{line}](#{line.replace(' ', '-').lower()})")
+            toc.append(f"{indent}- [{line}](#{jmp_anchor})")
 
     for k, v in enumerate(lines):
-        if v.find("TOC") >= 0 or v.find("toc") >= 0:
+        if v.find("-[TOC]") >= 0 or v.find("-[toc]") >= 0:
             lines[k] = "## Table of Contents\n" + "\n".join(toc) + "\n"
+            has_toc = True
             break
+
+    if not has_toc:
+        return
 
     with open(filename, "w", encoding="utf-8") as f:
         f.write("".join(lines))
+
+    print(f"generate toc for {filename}")
 
 
 if __name__ == "__main__":
