6623f5b1a148988f493c3c248d88443ee6352d91
Lewis Tian <taseikyo@gmail.com>
2020-10-29 11:40:34 +0800

    [py] update 00e
    - fix bug: zsh command (timestamp:exe-time;cmds)
    - add argparse to accept custom history filepath

--- Python/00E.remove_duplicate_history_cmds.py
+++ Python/00E.remove_duplicate_history_cmds.py
@@ -19,6 +19,7 @@ tian@node:~$ echo "/home/tian/remove_duplicate_cmds.py" >> .bashrc
 
 import os
 import time
+import argparse
 from datetime import datetime
 
 BASH_HISTORY_PATH = "/home/tian/.bash_history"
@@ -76,7 +77,7 @@ def zsh_history():
     with open(ZSH_HISTORY_PATH, encoding="utf-8") as f:
         for cmd in f:
             cnt += 1
-            purge_cmd = "".join(cmd.split(":")[2:])
+            purge_cmd = "".join(cmd.split(";")[1:])
             if purge_cmd in table:
                 continue
             table.add(purge_cmd)
@@ -87,10 +88,46 @@ def zsh_history():
         f.write("".join(cmds))
 
 
-if __name__ == "__main__":
+def main():
+    """
+    add argparse to accept custom history filepath
+
+    >>> python 00E.remove_duplicate_history_cmds.py --cmder "D:/Program Files/cmder/config/.history"
+    D:/Program Files/cmder/config/.history 297 -> 277
+    >>> python 00E.remove_duplicate_history_cmds.py --cmder "D:/Program Files/cmder/config/.history" --threshold 10
+    D:/Program Files/cmder/config/.history 277 -> 277
+    """
+    parser = argparse.ArgumentParser(description="remove duplicate history cmds")
+    parser.add_argument("--bash", help="filepath of bash history commands")
+    parser.add_argument("--zsh", help="filepath of zsh history commands")
+    parser.add_argument("--cmder", help="filepath of cmder history commands")
+    parser.add_argument(
+        "--threshold", type=int, help="time threshold for removing history"
+    )
+    args = parser.parse_args()
+    bash_history_path = args.bash
+    zsh_history_path = args.zsh
+    cmder_history_path = args.cmder
+    threshold = args.threshold
+    global BASH_HISTORY_PATH
+    global ZSH_HISTORY_PATH
+    global CMDER_HISTORY_PATH
+    global THRESHOLD_SECONDS
+    if bash_history_path:
+        BASH_HISTORY_PATH = bash_history_path
+    if zsh_history_path:
+        ZSH_HISTORY_PATH = zsh_history_path
+    if cmder_history_path:
+        CMDER_HISTORY_PATH = cmder_history_path
+    if threshold:
+        THRESHOLD_SECONDS = threshold
     if os.path.exists(BASH_HISTORY_PATH):
         cmd_history(BASH_HISTORY_PATH)
     if os.path.exists(ZSH_HISTORY_PATH):
         zsh_history()
     if os.path.exists(CMDER_HISTORY_PATH):
         cmd_history(CMDER_HISTORY_PATH)
+
+
+if __name__ == "__main__":
+    main()
